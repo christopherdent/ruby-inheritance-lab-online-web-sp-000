3302c0d38e30ab0e545fb05ead7c77cad783c49d
Christopher Dent <christopherdent01@gmail.com>
2019-09-19 01:59:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,13 +2,13 @@ class Student < User
   attr_accessor :first_name, :last_name
 
   def initialize
-    
     @knowledge = []
-    
   end 
   
   
-
+  def learn(knowledge)
+    @knowledge << knowledge 
+  end 
 
 
 end
